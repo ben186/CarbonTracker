3b41099e50d73a4f608acea24dcc67630cacb2d1
Paul Ruben <paulruben186@gmail.com>
2023-04-09 14:00:14 +0800

    Remove unused translations

--- app/i18n/en.ts
+++ app/i18n/en.ts
@@ -5,10 +5,9 @@ const en = {
     back: "Back",
   },
   welcomeScreen: {
-    postscript:
-      "psst  — This probably isn't what your app looks like. (Unless your designer handed you these screens, and in that case, ship it!)",
-    readyForLaunch: "Your app, almost ready for launch!",
-    exciting: "(ohh, this is exciting!)",
+    postscript: "BAKA",
+    readyForLaunch: "28% on your credit today!!",
+    exciting: "(ohh, this is bankruptcy!)",
   },
   errorScreen: {
     title: "Something went wrong!",
