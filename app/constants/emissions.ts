4915b6fe4fe847a41f6d7e7956df13f0e6187b28
Paul Ruben <paulruben186@gmail.com>
2023-06-03 18:58:39 +0800

    Add emission constants

---
+++ app/constants/emissions.ts
@@ -0,0 +1,6 @@
+export const EMISSIONS = [
+    { category: "car", factor: 24, unit: "km", icon: "car-sport-outline" },
+    { category: "electricity", factor: 11, unit: "kW", icon: "bulb-outline" },
+    { category: "food", factor: 55, unit: "g", icon: "pizza-outline" },
+    { category: "custom", factor: 1, unit: "g", icon: "leaf-outline" }
+]
