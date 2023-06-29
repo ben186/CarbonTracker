646183f6a63fc84b79179fdf7febaa509462420c
Paul Ruben <paulruben186@gmail.com>
2023-06-29 17:25:49 +0800

    Add gibberish for about page

--- app/screens/AboutScreen.tsx
+++ app/screens/AboutScreen.tsx
@@ -1,16 +1,17 @@
 import React, { FC } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
+import { Text, TextStyle, ViewStyle } from "react-native"
 import { AppBottomTabScreenProps } from "app/navigators"
-import { Text } from "app/components"
 import { SafeAreaView } from "react-native-safe-area-context"
 
 interface AboutScreenProp extends AppBottomTabScreenProps<"About"> {}
 
+const ABOUT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
 export const AboutScreen: FC<AboutScreenProp> = observer(function AboutScreen() {
   return (
     <SafeAreaView style={$root} mode="margin">
-      <Text>About</Text>
+      <Text style={$about}>{ABOUT}</Text>
     </SafeAreaView>
   )
 })
@@ -18,6 +19,10 @@ export const AboutScreen: FC<AboutScreenProp> = observer(function AboutScreen() 
 const $root: ViewStyle = {
   flex: 1,
   flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center"
+  paddingHorizontal: "2%"
+}
+
+const $about: TextStyle = {
+  fontSize: 16,
+  textAlign: "justify"
 }
