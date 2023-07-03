75bcc3506dbbda3931129a709e57ef93bf3e3845
Paul Ruben <paulruben186@gmail.com>
2023-07-03 19:18:56 +0800

    Revise emission factors

--- app/constants/emissions.ts
+++ app/constants/emissions.ts
@@ -9,8 +9,8 @@ export type EMISSION = {
 
 export const EMISSIONS: EMISSION[]
     = [
-        { category: "car", factor: 2, unit: "km", icon: "car-sport-outline" },
-        { category: "electricity", factor: 3, unit: "kW", icon: "bulb-outline" },
-        { category: "food", factor: 2, unit: "kg", icon: "pizza-outline" },
+        { category: "car", factor: 0.147, unit: "km", icon: "car-sport-outline" },
+        { category: "electricity", factor: 0.662, unit: "kW", icon: "bulb-outline" },
+        { category: "meal", factor: 1.405, unit: "kg", icon: "fast-food-outline" },
         { category: "custom", factor: 1, unit: "kg", icon: "leaf-outline" }
     ]
