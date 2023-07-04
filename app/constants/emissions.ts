a3adf02d40d644396e204e887e42bcae77c96129
Paul Ruben <paulruben186@gmail.com>
2023-07-04 13:58:34 +0800

    Change to kWh

--- app/constants/emissions.ts
+++ app/constants/emissions.ts
@@ -10,7 +10,7 @@ export type EMISSION = {
 export const EMISSIONS: EMISSION[]
     = [
         { category: "car", factor: 0.147, unit: "km", icon: "car-sport-outline" },
-        { category: "electricity", factor: 0.662, unit: "kW", icon: "bulb-outline" },
+        { category: "electricity", factor: 0.662, unit: "kWh", icon: "bulb-outline" },
         { category: "meal", factor: 1.405, unit: "meal(s)", icon: "fast-food-outline" },
         { category: "custom", factor: 1, unit: "kg", icon: "leaf-outline" }
     ]
